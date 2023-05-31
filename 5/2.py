aa090c86261190841a6bcc39b3412164e8bd3e64
yevgeniy2504 <84036529+yevgeniy2504@users.noreply.github.com>
2023-05-31 12:19:03 +0600

    Lesson_5

---
+++ 5/2.py
@@ -0,0 +1,11 @@
+# Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
+# В результирующем списке не должно быть дубликатов.
+
+
+init_list = [1, 1, 2, 3, 4, 4, 5, 5, 5, 6, 6, 7, 8, 9, ]
+
+repeated_list = [elem for elem in init_list if init_list.count(elem) > 1]
+print(list(set(repeated_list)))
+
+
+
